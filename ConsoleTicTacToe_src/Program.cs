ce17b45fd8b3d7f998572971553bd53cf879d693
Kicktys <106908348+Kicktys@users.noreply.github.com>
2024-10-02 15:45:42 +0300

    CellPosition method and basic variables
    basic variables arre added and CellPosition method

--- ConsoleTicTacToe_src/Program.cs
+++ ConsoleTicTacToe_src/Program.cs
@@ -4,11 +4,35 @@ namespace ConsoleTicTacToe
 {
     internal class Program
     {
-        static int a;
+        static int col = 0;
+        static int row = 0;
 
         static void Main(string[] args)
         {
+            char[,] CellContainer[col, row];
+        }
 
+        static void CellPosition(int col, int row)
+        {
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.UpArrow:
+                    col--;
+                   return;
+
+                case ConsoleKey.DownArrow:
+                    col++;
+                   return;
+
+                case ConsoleKey.LeftArrow:
+                    row--;
+                   return;
+
+                case ConsoleKey.RightArrow:
+                    row++;
+                   return;
+
+            }
         }
     }
 }
