c44e2e1fa551980ac4eca1f1a379956300073f19
mersedel <xr6033851@gmail.com>
2024-10-02 21:48:23 +0300

    HotFix + Displaying

--- ConsoleTicTacToe_src/Program.cs
+++ ConsoleTicTacToe_src/Program.cs
@@ -1,49 +1,74 @@
 ﻿using System;
-
 namespace ConsoleTicTacToe
 {
     internal class Program
     {
-        static int col = 0;
-        static int row = 0;
+        static byte selectedFieldRow;
+        static byte selectedFieldColumn;
+
+        static bool gameIsRunning = true; //тимчасово воно буде true на початку
+
+        static char[,] Field = 
+        {
+            { '_', '_', '_' },
+            { '_', '_', '_' },
+            { '_', '_', '_' }
+        };
+
 
         static void Main(string[] args)
         {
-            char[,] CellContainer[col, row];
+            while (gameIsRunning)
+            {
+                Console.Clear();
+                DrawField();
+                FieldCellSelecter();
+            }
         }
 
-        static void CellPosition(int col, int row)
+        static void DrawField()
         {
-            switch (Console.ReadKey().Key)
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (row == selectedFieldRow && col == selectedFieldColumn)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkGray;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write($"|{Field[row, col]}|");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write($"|{Field[row, col]}|");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void FieldCellSelecter()
+        {
+            switch (Console.ReadKey(true).Key)
             {
                 case ConsoleKey.UpArrow:
-                    col--;
-                   return;
+                    if (selectedFieldRow == 0) break;
+                    selectedFieldRow--; break;
 
                 case ConsoleKey.DownArrow:
-                    col++;
-                   return;
+                    if (selectedFieldRow == 2) break;
+                    selectedFieldRow++; break;
 
                 case ConsoleKey.LeftArrow:
-                    row--;
-                   return;
+                    if (selectedFieldColumn == 0) break;
+                    selectedFieldColumn--; break;
 
                 case ConsoleKey.RightArrow:
-                    row++;
-                   return;
-
+                    if (selectedFieldColumn == 2) break;
+                    selectedFieldColumn++; break;
             }
-        }*/
+        }
 
-        static string Output(int col, int row)
-        {
-            for (int columnIndex = 0; columnIndex < col; columnIndex++)
-            {
-                for (int rowIndex = 0; rowIndex < row; rowIndex++)
-                {
-                    Console.WriteLine(CellPosition(columnIndex, rowIndex));
-                }
-                rowIndex = 0;
-            }
     }
 }
