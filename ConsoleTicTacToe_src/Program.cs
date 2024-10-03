14f5ddad4736325b9846ba4bc34fca84318b7bd0
mersedel <xr6033851@gmail.com>
2024-10-03 23:02:00 +0300

    Added win checking + restart logic

--- ConsoleTicTacToe_src/Program.cs
+++ ConsoleTicTacToe_src/Program.cs
@@ -30,7 +30,7 @@ namespace ConsoleTicTacToe
 
         static void DrawField()
         {
-            Console.WriteLine($"{currentPlayer} Player`s turn:");
+            Console.WriteLine(currentPlayer + " Player`s turn:");
 
             for (int row = 0; row < 3; row++)
             {
@@ -73,12 +73,72 @@ namespace ConsoleTicTacToe
                     selectedFieldColumn++; break;
 
                 case ConsoleKey.Enter:
+                    if (Field[selectedFieldRow, selectedFieldColumn] != '_') break;
+
                     Field[selectedFieldRow, selectedFieldColumn] = currentPlayer;
 
-                    if (currentPlayer == 'X') { currentPlayer = 'O'; break; }
-                    if (currentPlayer == 'O') { currentPlayer = 'X'; break; }
+                    Console.Clear();
+                    DrawField();
+
+                    WinCheck();
+
+                    if (currentPlayer == 'X')
+                        currentPlayer = 'O';
+                    else
+                        currentPlayer = 'X';
+
                     break;
             }
+        }
+
+        static void WinCheck()
+        {
+            if (CheckLine(0, 0, 0, 1, 0, 2) || CheckLine(1, 0, 1, 1, 1, 2) || CheckLine(2, 0, 2, 1, 2, 2) || // Rows
+                CheckLine(0, 0, 1, 0, 2, 0) || CheckLine(0, 1, 1, 1, 2, 1) || CheckLine(0, 2, 1, 2, 2, 2) || // Columns
+                CheckLine(0, 0, 1, 1, 2, 2) || CheckLine(2, 0, 1, 1, 0, 2))                                 // Diagonals
+            {
+                Console.WriteLine(currentPlayer + " Wins!\n\nStart new game? (y/n)");
+
+                ConsoleKey key;
+                do
+                {
+                    key = Console.ReadKey(true).Key;
+
+                    if (key == ConsoleKey.Y)
+                    {
+                        ResetGame();
+                        break;
+                    }
+                    else if (key == ConsoleKey.N)
+                    {
+                        gameIsRunning = false;
+                        break;
+                    }
+                } while (key != ConsoleKey.Y && key != ConsoleKey.N);
+
+            }
+        }
+
+        static bool CheckLine(int r1, int c1, int r2, int c2, int r3, int c3)
+        {
+            return Field[r1, c1] == currentPlayer && Field[r2, c2] == currentPlayer && Field[r3, c3] == currentPlayer;
+        }
+
+        static void ResetGame()
+        {
+            Field = new char[3, 3]
+            {
+                { '_', '_', '_' },
+                { '_', '_', '_' },
+                { '_', '_', '_' }
+            };
+
+            currentPlayer = 'X';
+
+            selectedFieldRow = 0;
+            selectedFieldColumn = 0;
+
+            Console.Clear();
         }
     }
 }
